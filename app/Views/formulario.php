fa2f6ab01fe3cc563c3d0206a31f60974585b3ac
caradophp <contatolucianofriebe@gmail.com>
2025-02-23 13:07:45 -0300

    Animação de carregamento adicionada à tela de cadastro

--- app/Views/formulario.php
+++ app/Views/formulario.php
@@ -5,6 +5,15 @@
         <input type="text" name="nome" id="nome" class="form-control"><br><br>
         <label for="senha">Senha:</label>
         <input type="password" name="senha" id="senha" class="form-control"><br><br>
+        <center>
+        {% if error %}
+            <div style="color: red; font-weight: bold;">
+                {{ error }}
+            </div>
+        {% endif %}
+            <div id="Message" style="color:red;"></div>
+        <div class="loader" style="display:none;" id="loader"></div>
+        </center>
         <input type="submit" value="enviar" class="btn btn-info" id="confirm">
     </form>
 </div>
