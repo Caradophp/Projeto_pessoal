bb5c0e980de1bc031c42ee42fa5d463042f12abb
caradophp <contatolucianofriebe@gmail.com>
2025-02-23 22:43:27 -0300

    Alterações no painel de administrador

--- app/Views/admin/painel.php
+++ app/Views/admin/painel.php
@@ -1,21 +1,12 @@
-<div class="teste">
-   <div class="lateral">
-        <a href="">Dashboard</a>
-        <a href="">Rendimentos</a>
-        <a href="">Funcinários</a>
-        <a href="">Demandas</a>
-        <a href="">Mais...</a>
-    </div>
-    <div class="grid-container">
-        <div class="box blue">Dashboard</div>
-        <div class="box green">Charts</div>
-        <div class="box yellow">Widgets</div>
-        <div class="box red">Tables</div>
-        <div class="box purple">Full Width</div>
-        <div class="box orange">Forms</div>
-        <div class="box darkblue">Buttons</div>
-        <div class="box lightblue">Elements</div>
-        <div class="box teal">Calendar</div>
-        <div class="box gold">Errors</div>
-    </div>
-</div>
+ <div class="grid-container">
+        <div class="box blue"><a href="">Dashboard</a></div>
+        <div class="box green"><a href="">Charts</a></div>
+        <div class="box yellow"><a href="">Widgets</a></div>
+        <div class="box red"><a href="">Tables</a></div>
+        <div class="box purple"><a href="">Full Width</a></div>
+        <div class="box orange"><a href="">Forms</a></div>
+        <div class="box darkblue"><a href="">Buttons</a></div>
+        <div class="box lightblue"><a href="">Elements</a></div>
+        <div class="box teal"><a href="">Calendar</a></div>
+        <div class="box gold"><a href="">Errors</a></div>
+ </div>
