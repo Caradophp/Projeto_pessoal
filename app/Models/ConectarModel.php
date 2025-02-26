359dd743537697092facb14ae24435907c833371
caradophp <contatolucianofriebe@gmail.com>
2025-02-25 21:46:17 -0300

    Removido erro das constantes

--- app/Models/ConectarModel.php
+++ app/Models/ConectarModel.php
@@ -9,10 +9,10 @@ require_once __DIR__ . '/../Middlewares/AuthMiddleware.php';
 
 class conectarModel {
 
-    private $host = DB_HOST;
-    private $name = DB_NAME;
-    private $user = DB_USER;
-    private $pass = DB_PASS;
+    // private $host = DB_HOST;
+    // private $name = DB_NAME;
+    // private $user = DB_USER;
+    // private $pass = DB_PASS;
 
     public function connect(){
 
