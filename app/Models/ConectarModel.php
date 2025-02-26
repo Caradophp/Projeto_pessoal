94e5adbb6ab10b81a9ceffd585dd4e66262d7823
caradophp <contatolucianofriebe@gmail.com>
2025-02-25 21:32:58 -0300

    Adicionada constantes de configuração do banco

--- app/Models/ConectarModel.php
+++ app/Models/ConectarModel.php
@@ -3,13 +3,19 @@
 namespace app\Models;
 
 use PDO;
+use app\functions\db_config;  
 
 class conectarModel {
+
+    private $host = DB_HOST;
+    private $name = DB_NAME;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
 
     public function connect(){
 
         try {
-            $conn = new PDO('mysql:dbname=users;host=localhost', 'root', '');
+            $conn = new PDO('mysql:dbname={$this->$host};host=localhost', "root", "");
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //echo "Conectado com sucesso!!!";
             return $conn;
---
+++ app/functions/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\functions;
+
+define("localhost", "DB_HOST");
+define("users", "DB_NAME");
+define("root", "DB_USER");
+define("", "DB_PASS");
